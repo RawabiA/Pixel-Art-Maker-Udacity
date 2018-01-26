25f627c0c57979597b2682f426861f41ad055005
Anna Ikonomova <33806976+anikonomova@users.noreply.github.com>
2018-01-26 10:52:24 +0200

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,26 +1,33 @@
-// Select color input
-// Select size input
+$(document).ready(function() {
+  const table = $("#pixel_canvas");
+  $("input:submit").click(function(event) {
+    event.preventDefault(); //prevents refresh
+    makeGrid(); //clicking the submit button calls the  makeGrid funktion
+  });
+  $("#clear").on("click", function(evt) {
+    $("#pixel_canvas").empty();
+  }); //clears the Grid
+  // Making the Grid
+  function makeGrid() {
+    var row = $("#input_height").val();
+    var column = $("#input_width").val();
 
-// When size is submitted by the user, call makeGrid()
-const canvas = $("#pixel_canvas");
-const colorPicker = $("#colorPicker");
-//Erase previous canvas
-function resetGrid(){
-    $('canvas').empty();
-}
-function makeGrid(){
-  var width = $("input_width").val();
-  var height = $("input_height").val();
-for (x = 0; x <= height; x++) {
-  let li = $("<tr></tr>");
-  $('canvas').append(li);
-  for (y = 0; y <= width; y++) {
-    li.append("<td></td>");
+    for (let x = 0; x < row; x++) {
+      table.append("<tr></tr>");
+      for (let y = 0; y < column; y++) {
+        $("tr")
+          .last()
+          .append("<td></td>");
+      }
+    }
   }
-}
-makeGrid(height.val(), width.val());
+  //Let's paint!
+  var paint = table.on("click", "td", function() {
+    var color = $("#colorPicker").val();
+    $(this).attr("bgcolor", color);
+  });
+  //the double click
+  var erase = table.on("dblclick", "td", function() {
+    $(this).attr("bgcolor", "#ffffff");
+  });
 });
-// Color
-var color = $("input[type='color']").val();
-}
-}
